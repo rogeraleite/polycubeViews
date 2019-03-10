9b0c9d9bf3e0accd5978c0d44a0b725ddee056ae
Saminu <s.salisu@live.co.uk>
2019-03-10 22:42:04 +0100

    added the map holder for geocube

--- src/app/classes/geocube.ts
+++ src/app/classes/geocube.ts
@@ -40,7 +40,18 @@ export class GeoCube implements PolyCube {
         // this.timeLinearScale(some_date) gives us the vertical axis coordinate of the point
         this.timeLinearScale = this.dm.getTimeLinearScale();
         let geometry = new THREE.SphereGeometry( 1, 32, 32 );
+
+        /**
+        * Array cube rotation and position css3d matrix
+            * @type {*[]}
+        */
+        let r:number = Math.PI / 2;
+        let d = 250;
+        let pos = [[d, 0, 0], [-d, 0, 0], [0, d, 0], [0, -d, 0], [0, 0, d], [0, 0, -d]];
+        let rot = [[0, r, 0], [0, -r, 0], [-r, 0, 0], [r, 0, 0], [0, 0, 0], [0, 0, 0]];
+
         
+
         for(let i = 0; i < this.dm.data.length; i++) {
             let dataItem = this.dm.data[i];
             // TODO: consider just updating color property of material if you ever find out how to do it
@@ -53,6 +64,40 @@ export class GeoCube implements PolyCube {
 
             this.cubeGroupGL.add(sphere);
         }
+
+
+        // this.dm.data.forEach((d:any)=> {
+        //     let div = new THREE.CSS3DObject(element);
+        //     // div.position.x = 8;
+        //     // div.position.y = 9;
+        //     // div.position.z = 185;
+
+        //     // div.position.y = this.timeLinearScale(d.date_time);
+        //     // div.position.x = Math.random()*CUBE_CONFIG.WIDTH;
+        //     // div.position.z = Math.random()*CUBE_CONFIG.WIDTH;
+        //     // this.cubeGroupCSS.add(div)
+        // })
+
+        //flat layer
+        //HTML
+        let element = document.createElement('div');
+        element.innerHTML = 'Map Holder';
+        element.style.background = "#0094ff";
+        element.style.width = CUBE_CONFIG.WIDTH + "px";
+        element.style.height = CUBE_CONFIG.WIDTH  + "px";
+        element.className = 'csspc' ;
+        element.style.color = "white";
+        // element.style.padding = "2em";
+
+        // CSS Object
+        let div = new THREE.CSS3DObject(element);
+        div.position.x = CUBE_CONFIG.WIDTH/2;
+        div.position.y = -CUBE_CONFIG.WIDTH/2;
+        div.position.z = CUBE_CONFIG.WIDTH/2;
+        // div.position.fromArray(pos[2]);
+        div.rotation.fromArray(rot[2]);
+        this.cubeGroupCSS.add(div)
+
     }
 
     render(): void {
