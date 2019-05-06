cc2a7a45c7eb6b63412d68d4e624f628c4cdcfd5
Velitchko Filipov <velitchkofilipov@gmail.com>
2019-05-06 14:19:08 +0200

    node size set to minimum

--- src/app/cube.config.js
+++ src/app/cube.config.js
@@ -2,7 +2,7 @@ export const CUBE_CONFIG = {
     WIDTH: 500,
     HEIGHT: 500,
     GUTTER: 200,
-    NODE_SIZE: 4,
+    NODE_SIZE: 2,
     DATA_SET: {
         id: '1j-FnypM3zD2fjWWoZUa_X6ENh4LosKF627fZoXKSxpY',
         name: 'Cushman'
