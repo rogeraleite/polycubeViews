3130169c3f5f56727d86720e80f61038971c45d7
Velitchko Filipov <velitchkofilipov@gmail.com>
2019-09-17 15:41:57 +0200

    importing new services and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// Routing
+import { AppRoutingModule } from './app.routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { GoogleDriveProvider } from './services/google.drive.service';
-import { AppComponent } from './app.component';
+import { GraphPositionService } from './services/graph.position.service';
+import { DataManager } from './classes/datamanager';
+import { AppComponent } from './app.component'; 
+import { CubeComponent } from './components/cube.component/cube.component';
 import { TimeSliderComponent } from './components/timeslider.component/timeslider';
+import { ForceDirectedComponent } from './util/forceDirectedSimulation/forceDirectedSimulation';
 import { SidebarModule } from 'ng-sidebar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TimeSliderComponent
+    CubeComponent,
+    TimeSliderComponent,
+    ForceDirectedComponent
   ],
   imports: [
     BrowserModule,
     SidebarModule.forRoot(),
+    AppRoutingModule,
     HttpClientModule
   ],
-  providers: [GoogleDriveProvider],
+  providers: [GoogleDriveProvider, GraphPositionService, DataManager],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
