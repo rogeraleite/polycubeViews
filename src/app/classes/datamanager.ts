ba5c54b85b494645b11ed2e1e6c5a5a3b91263d2
Velitchko Filipov <velitchkofilipov@gmail.com>
2019-09-17 15:39:29 +0200

    datamanager converted to a service

--- src/app/classes/datamanager.ts
+++ src/app/classes/datamanager.ts
@@ -1,40 +1,39 @@
 import * as D3 from 'd3';
 import * as moment from 'moment';
 import { CUBE_CONFIG } from '../cube.config';
-import { CushmanForcedDirected } from '../../data/cushman_nodes_position';
+import { GraphPositionService } from '../services/graph.position.service';
+import { Injectable } from '@angular/core';
 
-
+@Injectable()
 export class DataManager {
     private _data: Array<any>;
-    private _data_map: Map<any,any>;
-    private _cushman_pos: CushmanForcedDirected;
+    private _data_map: Map<any, any>;
 
     private _numSlices: number
     private _timeRange: Array<Date>;
     private timeLinearScale: D3.ScaleLinear<number, number>;
-    
+
     private MIN_DATE: Date;
     private MAX_DATE: Date;
 
     private _categories: Map<string, string>;
     private _colors: D3.ScaleOrdinal<string, string>;
 
-    constructor() {
+    constructor(private gps: GraphPositionService) {
         this._data_map = new Map();
-        this._cushman_pos = new CushmanForcedDirected();
         this._numSlices = 10;
         this._timeRange = new Array<Date>();
 
         this._categories = new Map<string, string>();
         this._colors = D3.scaleOrdinal([
-            '#e6194b', '#f58231', '#ffe119', '#bfef45','#3cb44b', 
-            '#808000','#42d4f4', '#4363d8', '#911eb4', '#f032e6', 
-            '#fabebe', '#ffd8b1', '#9a6324', '#800000', '#e6beff', 
-            '#aaffc3', '#469990', '#000075', '#808080','#000000', 
+            '#e6194b', '#f58231', '#ffe119', '#bfef45', '#3cb44b',
+            '#808000', '#42d4f4', '#4363d8', '#911eb4', '#f032e6',
+            '#fabebe', '#ffd8b1', '#9a6324', '#800000', '#e6beff',
+            '#aaffc3', '#469990', '#000075', '#808080', '#000000',
         ]);
     }
 
-    get dataMap(): Map<any,any> {
+    get dataMap(): Map<any, any> {
         return this._data_map;
     }
 
@@ -53,10 +52,10 @@ export class DataManager {
 
         this._timeRange = D3.scaleTime().domain([this.MIN_DATE, this.MAX_DATE]).ticks(this._numSlices);
         this.timeLinearScale = D3.scaleLinear()
-                                 .domain([this.MIN_DATE, this.MAX_DATE])
-                                 .range([-CUBE_CONFIG.WIDTH/2, CUBE_CONFIG.WIDTH/2]);
+            .domain([this.MIN_DATE, this.MAX_DATE])
+            .range([-CUBE_CONFIG.WIDTH / 2, CUBE_CONFIG.WIDTH / 2]);
 
-        
+
         this.createDataMap();
         this.createCategories();
     }
@@ -74,20 +73,20 @@ export class DataManager {
     }
 
     getNodeById(id: string): any {
-    return this._data.find((d: any) => { 
-        return d.id == id; 
-    });
+        return this._data.find((d: any) => {
+            return d.id == id;
+        });
     }
 
     createCategories(): void {
-        this._data.forEach((d: any) => { 
-            if(this._categories.has(d.category_1)) return;
+        this._data.forEach((d: any) => {
+            if (this._categories.has(d.category_1)) return;
             this._categories.set(d.category_1, this._colors(d.category_1));
         });
     }
 
-    createDataMap(): any{
-        if(this._data){
+    createDataMap(): any {
+        if (this._data) {
             this._data_map = new Map();
             this._data.forEach(e => {
                 this._data_map[e.id] = e;
@@ -95,14 +94,14 @@ export class DataManager {
         }
     }
 
-    getForcedDirectedCushmanPositionMap(): any {                
-        return this._cushman_pos.nodesPosMap;
+    getForcedDirectedPositionMap(): any {
+        return this.gps.nodesPosMap;
     }
 
-    getDataPositionDimensions(): any{
-        return this._cushman_pos.getDataPositionDimensions(); 
+    getDataPositionDimensions(): any {
+        return this.gps.getDataPositionDimensions();
     }
-    
+
     get numSlices(): number {
         return this._numSlices;
     }
@@ -119,8 +118,8 @@ export class DataManager {
 
         this._timeRange = D3.scaleTime().domain([this.MIN_DATE, this.MAX_DATE]).ticks(this._numSlices);
         this.timeLinearScale = D3.scaleLinear()
-                                 .domain([this.MIN_DATE, this.MAX_DATE])
-                                 .range([-CUBE_CONFIG.WIDTH/2, CUBE_CONFIG.WIDTH/2]);
+            .domain([this.MIN_DATE, this.MAX_DATE])
+            .range([-CUBE_CONFIG.WIDTH / 2, CUBE_CONFIG.WIDTH / 2]);
     }
 
     getMinDate(): Date { return this.MIN_DATE; }
@@ -135,11 +134,11 @@ export class DataManager {
             return d.date_time;
         });
     }
-    
+
     // deprecated
     // getTimeslice(date: Date): number {
     //     let dateDiff = moment(this.MAX_DATE).diff(this.MIN_DATE, 'days');
-        
+
     //     console.log(dateDiff/this._numSlices);
 
     //     return 0;
@@ -152,22 +151,22 @@ export class DataManager {
         // https://stackoverflow.com/questions/24541296/d3-js-time-scale-nicely-spaced-ticks-at-minute-intervals-when-data-is-in-second
 
         this._timeRange = D3.scaleTime().domain([this.MIN_DATE, this.MAX_DATE]).ticks(this._numSlices);
-        
+
         // TODO: Consider temporal granularity (currently in years) -> days?
         let myQuantizeFunction = D3.scaleQuantize()
-                                   .domain([this.MIN_DATE, this.MAX_DATE])
-                                   .range( this._timeRange.map((d) => { return d.getFullYear(); }));
+            .domain([this.MIN_DATE, this.MAX_DATE])
+            .range(this._timeRange.map((d) => { return d.getFullYear(); }));
 
         return myQuantizeFunction(date);
     }
 
-    getCustomTimeQuantile(date: Date, slice:number = this._numSlices): any {
-        let timeRange  = D3.scaleTime().domain([this.MIN_DATE, this.MAX_DATE]).ticks(slice);
-    
+    getCustomTimeQuantile(date: Date, slice: number = this._numSlices): any {
+        let timeRange = D3.scaleTime().domain([this.MIN_DATE, this.MAX_DATE]).ticks(slice);
+
         // TODO: Consider temporal granularity (currently in years) -> days?
         let myQuantizeFunction = D3.scaleQuantize()
-                                   .domain([this.MIN_DATE, this.MAX_DATE])
-                                   .range( timeRange.map((d) => { return d.getFullYear(); }));
+            .domain([this.MIN_DATE, this.MAX_DATE])
+            .range(timeRange.map((d) => { return d.getFullYear(); }));
 
         return myQuantizeFunction(date);
     }
